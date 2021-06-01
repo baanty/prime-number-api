12db28c35f40f413761e3150df0844de08495c1c
pijushdas <pijushdas@gmail.com>
2021-06-02 00:58:53 +0530

    Return XML or JSON.

--- src/main/java/com/pijush/prime/presentation/controller/PrimeController.java
+++ src/main/java/com/pijush/prime/presentation/controller/PrimeController.java
@@ -3,12 +3,15 @@
  */
 package com.pijush.prime.presentation.controller;
 
+import java.math.BigInteger;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pijush.prime.common.vo.Constants;
 import com.pijush.prime.common.vo.PrimeJsonResponse;
@@ -27,8 +30,9 @@ import com.pijush.prime.common.vo.jaxb.PrimeResponse;
 @Controller
 public class PrimeController implements Constants {
 
-	@GetMapping(value = "/{input}" , produces = { MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE })
-	public PrimeResponseMarker getPrimeNumbersInJsonormat(final @PathVariable String anIntegerString,
+	@GetMapping(value = "/{anIntegerString}" , produces = { MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+	public PrimeResponseMarker getPrimeNumbersInJsonFormat(final @PathVariable("anIntegerString") String anIntegerString,
 														  final @RequestParam("mediaType") String mediaType) {
 		PrimeJsonResponse aPrimeJsonResponse = new PrimeJsonResponse();
 		
@@ -36,6 +40,9 @@ public class PrimeController implements Constants {
 			return aPrimeJsonResponse;
 		} else if ( mediaType.equalsIgnoreCase(XML) ) {
 			PrimeResponse aPrimeResponseForXml = new PrimeResponse();
+			aPrimeResponseForXml.setInitial(anIntegerString);
+			aPrimeResponseForXml.getPrimes().add(new BigInteger("100"));
+			aPrimeResponseForXml.getPrimes().add(new BigInteger("300"));
 			return aPrimeResponseForXml;
 		}
 		return aPrimeJsonResponse;
